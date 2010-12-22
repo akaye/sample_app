87067526a8ed12f5cdbaa73cdb52cfe2b04be096
Alfred Kaye <alfred.kaye@gmail.com>
2010-12-22 11:57:17 -0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SampleApp::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
-
+  get "page/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
